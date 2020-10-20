0e14afd8cda1058814c0ae37ac866d4b0d4d3e48
ClaudiaAM <caldama062017@gmail.com>
2020-10-20 16:12:44 +0200

    Base InfoWindow

--- app/src/main/java/app/paseico/MainActivity.java
+++ app/src/main/java/app/paseico/MainActivity.java
@@ -2,36 +2,46 @@ package app.paseico;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
-
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener{
 
     GoogleMap map;
+    Marker marker;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            map = googleMap;
+            LatLng m1 = new LatLng(20, 40);
+            marker = map.addMarker(new MarkerOptions()
+                    .position(m1)
+                    .draggable(true)
+                    .title("My marker")
+                    .snippet("Lat")
+            );
+            map.setOnInfoWindowClickListener(this);
+            map.moveCamera(CameraUpdateFactory.newLatLng(m1));
+        }
+
+        @Override
+        public void onInfoWindowClick(Marker marker) {
+            Toast.makeText(this, "Info Window is clicked", Toast.LENGTH_LONG).show();
+        }
     }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-
-        LatLng Maharashtra = new LatLng(19.169257, 73.341601);
-        map.addMarker(new MarkerOptions().position(Maharashtra).title("Maharashtra"));
-        map.moveCamera(CameraUpdateFactory.newLatLng(Maharashtra));
-    }
-}
