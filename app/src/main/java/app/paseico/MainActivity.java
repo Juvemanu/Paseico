4181791a5baccff424e93e47a021c2dd771b55db
ClaudiaAM <caldama062017@gmail.com>
2020-10-20 22:31:41 +0200

    InfoWindow finished

--- app/src/main/java/app/paseico/MainActivity.java
+++ app/src/main/java/app/paseico/MainActivity.java
@@ -2,7 +2,6 @@ package app.paseico;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
-
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -16,10 +15,13 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+import android.content.Intent;
+import android.net.Uri;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import android.content.ComponentName;
+
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener{
 
@@ -68,12 +70,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         marker.setTitle(title);
-        marker.setSnippet(city);
+        marker.setSnippet(city + "\nClick aquí para ver en Google Maps");
     }
 
     @Override
     public void onInfoWindowClick(Marker marker) {
-        Toast.makeText(this, "Info Window is clicked", Toast.LENGTH_LONG).show();
-        marker.showInfoWindow();
+        Toast.makeText(this, "Redirigiendo a Google Maps", Toast.LENGTH_LONG).show();
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://maps.google.com?q=" + EXAMPLE.latitude +"," + EXAMPLE.longitude));
+        startActivity(browserIntent);
     }
 }
